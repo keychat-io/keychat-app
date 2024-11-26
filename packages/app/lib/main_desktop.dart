ceef169e3167e4e2b00b4f46d3e50b9340e8f2b6
ares <btc001@tuta.io>
2024-11-26 14:52:55 +0800

    fix

--- packages/app/lib/main_desktop.dart
+++ packages/app/lib/main_desktop.dart
@@ -567,7 +567,7 @@ class _ItemList extends StatelessWidget {
                   child: CircleAvatar(
                     radius: 18,
                     child: Image.asset(
-                      'images/logo.png',
+                      'assets/images/logo.png',
                       width: 100,
                       height: 100,
                       fit: BoxFit.cover,
@@ -1057,7 +1057,7 @@ const List<_Item> _allItems = <_Item>[
       _Email(
         sender: 'So Duri',
         recipients: 'me, Ziad and Lily',
-        image: 'images/logo.png',
+        image: 'assets/images/logo.png',
         time: '20 min',
         body:
             'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec gravida tellus, vel scelerisque nisi. Mauris egestas, augue nec dictum tempus, diam sapien luctus odio, a posuere sem neque at nulla. Vivamus pulvinar nisi et dapibus dapibus. Donec euismod pellentesque ultrices. Vivamus quis condimentum metus, in venenatis lorem. Proin suscipit tincidunt eleifend. Praesent a nisi ac ipsum sodales gravida.',
@@ -1066,7 +1066,7 @@ const List<_Item> _allItems = <_Item>[
       _Email(
           sender: 'Me',
           recipients: 'me, Ziad, and Lily',
-          image: 'images/logo.png',
+          image: 'assets/images/logo.png',
           time: '4 min',
           body:
               'Donec non mollis nulla, in varius mi. Ut id lorem eget felis lobortis tincidunt. Curabitur facilisis ex vitae tristique efficitur. Aenean eget augue finibus, tempor eros vitae, tempor neque. In sed pellentesque elit. Donec lacus lacus, malesuada in tincidunt sit amet, condimentum vel enim. Cras dapibus erat quis nisl hendrerit, vel pretium turpis condimentum. ',
@@ -1074,7 +1074,7 @@ const List<_Item> _allItems = <_Item>[
       _Email(
           sender: 'Ziad Aouad',
           recipients: 'me, Ziad and Lily',
-          image: 'images/logo.png',
+          image: 'assets/images/logo.png',
           time: '2 min',
           body:
               'Duis sit amet nibh a diam placerat aliquam nec ac mi. Aenean hendrerit efficitur tellus, non pharetra eros posuere sit amet. Maecenas interdum lacinia eleifend. Nam efficitur tellus et dolor vestibulum, non dictum quam iaculis. Aenean id nulla ut erat placerat feugiat. Mauris in quam metus. Aliquam erat volutpat.',
@@ -1089,8 +1089,8 @@ const List<_Item> _allItems = <_Item>[
         time: '2 hours',
         body:
             'Curabitur tincidunt purus at vulputate mattis. Nam lectus urna, varius eget quam in, ultricies ultrices libero. Curabitur rutrum ultricies varius. Orci varius natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec vulputate auctor est, non semper velit eleifend sit amet.',
-        image: 'images/logo.png',
-        bodyImage: 'images/logo.png',
+        image: 'assets/images/logo.png',
+        bodyImage: 'assets/images/logo.png',
         recipients: '',
       ),
     ],
@@ -1103,7 +1103,7 @@ const List<_Item> _allItems = <_Item>[
         time: '10 min',
         body:
             'Cras egestas ultricies elit, vitae interdum lorem aliquam et. Donec quis arcu a quam tempor rutrum vitae in lectus. Nullam elit nunc, lacinia sed luctus non, mollis id nulla. Morbi luctus turpis sapien, id molestie ante maximus vel. Vivamus sagittis consequat nisl nec placerat.',
-        image: 'images/logo.png',
+        image: 'assets/images/logo.png',
         bodyImage: '',
         recipients: '',
       ),

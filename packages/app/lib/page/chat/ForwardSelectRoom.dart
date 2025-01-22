b8640559639a6df04992786613687773882b92c5
ares <btc001@tuta.io>
2025-01-22 16:01:31 +0800

    Remove browser for android

--- packages/app/lib/page/chat/ForwardSelectRoom.dart
+++ packages/app/lib/page/chat/ForwardSelectRoom.dart
@@ -2,7 +2,6 @@ import 'package:app/controller/home.controller.dart';
 import 'package:app/models/room.dart';
 import 'package:app/page/chat/RoomUtil.dart';
 import 'package:app/utils.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 

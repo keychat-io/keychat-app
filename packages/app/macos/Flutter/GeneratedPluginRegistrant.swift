fad3e021d476d438d10ce1251a6c4ab9dead759b
kcdev <153359248+kcdev001@users.noreply.github.com>
2025-02-21 10:36:08 +0800

    config for build dmg (#80)
    * config for build dmg
    
    * bugfix and update readme
    
    * update readme

--- packages/app/macos/Flutter/GeneratedPluginRegistrant.swift
+++ packages/app/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,7 +5,9 @@
 import FlutterMacOS
 import Foundation
 
+import app_settings
 import connectivity_plus
+import file_picker
 import file_selector_macos
 import firebase_analytics
 import firebase_core
@@ -19,13 +21,15 @@ import package_info_plus
 import path_provider_foundation
 import share_plus
 import shared_preferences_foundation
-import sqflite
+import sqflite_darwin
 import url_launcher_macos
 import video_compress
 import video_player_avfoundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  AppSettingsPlugin.register(with: registry.registrar(forPlugin: "AppSettingsPlugin"))
   ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
   FLTFirebaseAnalyticsPlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseAnalyticsPlugin"))
   FLTFirebaseCorePlugin.register(with: registry.registrar(forPlugin: "FLTFirebaseCorePlugin"))
--- scripts/build_macos.sh
+++ scripts/build_macos.sh
@@ -1,20 +1,23 @@
 #!/bin/sh
 # Script Name: MacOS build
- 
-# flutter build macos -t lib/main.dart --obfuscate --split-debug-info=./docs/obfuscate/macos/
-# open macos/Runner.xcworkspace
-# https://pub.dev/packages/dmg
-# rm -rf build/macos && flutter build macos --release -v
-dart run dmg --sign-certificate "Developer ID Application: kai mei (HDBNSZBLMN)" --verbose --no-build
+
+# submissionid="9e6a195d-c400-4727-8702-552ec6e2bf6b"
+# output="packages/app/build/macos/Build/Products/Release/keychat.dmg"
+# xcrun notarytool history --keychain-profile "NotaryProfile"
+# xcrun notarytool info "${submissionid}" --keychain-profile "NotaryProfile"
+# ## staple
+# xcrun stapler staple -v $output
+
+cd packages/app/
+rm -rf build/macos && flutter build macos --release -v
+
+dart run dmg --sign-certificate "Developer ID Application: kai mei (HDBNSZBLMN)" --verbose --no-build --notary-profile "NotaryProfile"
 
 # output: build/macos/Build/Products/Release/<name>.dmg
 
-# create-dmg "xx/keychat.app" --identity "Developer ID Application: xx (xx)" --overwrite --dmg-title "keychat" packages/app/build/macos/Build/Products/Release
-
+## or export *.app from xcode and create dmg by create-dmg
+# click widown-> Organizer -> Archives -> validate app -> Export  -> keychat.app
 # create dmg by .app file
 # https://github.com/sindresorhus/create-dmg
 # npm install --global create-dmg
-# cd packages/app/
-# flutter build macos --release -v
-# open macos/Runner.xcworkspace
-# click widown-> Organizer -> Archives -> validate app -> Export  -> keychat.app
+# create-dmg 'path/to/.app' --overwrite --dmg-title='keychat' 'path/to/output/.dmg'

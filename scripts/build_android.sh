7aec6e89da9f6974714751d8aa887f7299aa863d
ares <btc001@tuta.io>
2025-06-06 12:21:33 +0800

    build config

--- scripts/build_android.sh
+++ scripts/build_android.sh
@@ -2,10 +2,9 @@
 # flutter build apk -v -t lib/main_prod.dart --target-platform android-arm --split-per-abi
 current_path=$(pwd)
 cd packages/app/
-flutter build apk --release -v -t lib/main.dart --dart-define=dart.vm.product=true --split-per-abi --obfuscate --split-debug-info=./build/obfuscate/apk/
+flutter build apk --release -v -t lib/main.dart
 
 output="$current_path/packages/app/build/app/outputs/apk/release"
 echo "Apk path: $output"
 open $output
 
-# flutter symbolize -d build/obfuscate/apk/app.android-arm64.symbols -i ./2025-04-21-error.log

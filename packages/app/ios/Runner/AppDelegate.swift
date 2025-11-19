134c2991b97b5c8e06941b9a2ff62c3408c96fe7
ares <btc001@tuta.io>
2025-11-19 18:34:20 +0800

    migrate appdelegate

--- packages/app/ios/Runner/AppDelegate.swift
+++ packages/app/ios/Runner/AppDelegate.swift
@@ -1,40 +1,45 @@
-import UIKit
 import Flutter
+import UIKit
 import app_links
 import receive_sharing_intent
 
 @main
-@objc class AppDelegate: FlutterAppDelegate {
+@objc class AppDelegate: FlutterAppDelegate, FlutterImplicitEngineDelegate {
   override func application(
     _ application: UIApplication,
     didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
   ) -> Bool {
-    GeneratedPluginRegistrant.register(with: self)
 
     // Retrieve the link from parameters
     if let url = AppLinks.shared.getLink(launchOptions: launchOptions) {
       // We have a link, propagate it to your Flutter app or not
       AppLinks.shared.handleLink(url: url)
-      return true // Returning true will stop the propagation to other packages
+      return true  // Returning true will stop the propagation to other packages
     }
 
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
   }
 
+  func didInitializeImplicitFlutterEngine(_ engineBridge: FlutterImplicitEngineBridge) {
+    GeneratedPluginRegistrant.register(with: engineBridge.pluginRegistry)
+  }
+
   // If the application is using multiple libraries, which needs to implement this function here in AppDelegate, you should check if the url is made from SwiftReceiveSharingIntentPlugin (if so, return the sharingIntent response) or call the handler of specific librabry
-    override func application(_ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey : Any] = [:]) -> Bool {
-        let sharingIntent = SwiftReceiveSharingIntentPlugin.instance
-        if sharingIntent.hasMatchingSchemePrefix(url: url) {
-            return sharingIntent.application(app, open: url, options: options)
-        }
-        
-         // For example load MSALPublicClientApplication
-         // return MSALPublicClientApplication.handleMSALResponse(url, sourceApplication: options[.sourceApplication] as? String)
-
-         // Cancel url handling
-         // return false
-
-         // Proceed url handling for other Flutter libraries like uni_links
-         return super.application(app, open: url, options:options)
+  override func application(
+    _ app: UIApplication, open url: URL, options: [UIApplication.OpenURLOptionsKey: Any] = [:]
+  ) -> Bool {
+    let sharingIntent = SwiftReceiveSharingIntentPlugin.instance
+    if sharingIntent.hasMatchingSchemePrefix(url: url) {
+      return sharingIntent.application(app, open: url, options: options)
     }
+
+    // For example load MSALPublicClientApplication
+    // return MSALPublicClientApplication.handleMSALResponse(url, sourceApplication: options[.sourceApplication] as? String)
+
+    // Cancel url handling
+    // return false
+
+    // Proceed url handling for other Flutter libraries like uni_links
+    return super.application(app, open: url, options: options)
+  }
 }
